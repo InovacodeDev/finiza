d7096e4600791e21c3ed70caa918698fd6e22a2c
Tito Motter <titoribeiromotter@gmail.com>
2026-02-27 08:59:40 -0300

    refactor: remove ProblemSection and AgitationSection components from the main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { HeroSection } from "@/components/sections/HeroSection";
-import { ProblemSection } from "@/components/sections/ProblemSection";
-import { AgitationSection } from "@/components/sections/AgitationSection";
 import { SolutionSection } from "@/components/sections/SolutionSection";
 import { FeatureSection } from "@/components/sections/FeatureSection";
 import { PricingSection } from "@/components/sections/PricingSection";
@@ -11,8 +9,6 @@ export default function Home() {
     return (
         <div className="w-full flex flex-col relative">
             <HeroSection />
-            <ProblemSection />
-            <AgitationSection />
             <SolutionSection />
             <FeatureSection />
             <PricingSection />
